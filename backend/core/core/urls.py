3fa058603bf0eb0a6bb1fac0af77beb2c13a93ec
Morteza <lomililmore@gmail.com>
2022-05-24 21:32:36 +0430

    updating the requirements.txt and removing unnecessery code from urls.py

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls.conf import include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +29,3 @@ if settings.DEBUG:
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
-if not settings.DEBUG:
-    urlpatterns.append(
-        re_path(".*", TemplateView.as_view(template_name="index.html")))

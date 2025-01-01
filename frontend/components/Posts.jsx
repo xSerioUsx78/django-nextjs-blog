b85d0724d83075749f407accd133b773e93af287
Emmanuel Makau <emmanuelmakau90@gmail.com>
2025-01-01 14:48:36 +0300

    replaced the a tags with the span tag to remove the hydrations error

--- frontend/components/Posts.jsx
+++ frontend/components/Posts.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
-
-const Posts = ({posts}) => {
+const Posts = ({ posts }) => {
   return (
     <>
       <div className="mb-4">
@@ -9,21 +8,14 @@ const Posts = ({posts}) => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {posts.map((post) => (
-          <div key={post.id} className="
-          relative
-          rounded-xl
-          shadow-md
-          transition 
-          duration-300 
-          p-4 
-          border 
-          border 
-          border-gray-600
-          hover:border-blue-400">
-            <h2 className="text-lg text-blue-400 text-bold mb-2">{post.title}</h2>
+          <div
+            key={post.id}
+            className="relative rounded-xl shadow-md transition duration-300 p-4 border border-gray-600 hover:border-blue-400"
+          >
+            <h2 className="text-lg text-blue-400 font-bold mb-2">{post.title}</h2>
             <p>{post.content}</p>
-            <Link href={`/posts/${post.id}`} >
-              <a className="absolute w-full h-full top-0 left-0"></a>
+            <Link href={`/posts/${post.id}`}>
+              <span className="absolute w-full h-full top-0 left-0"></span>
             </Link>
           </div>
         ))}
@@ -31,5 +23,5 @@ const Posts = ({posts}) => {
     </>
   );
 }
- 
+
 export default Posts;
